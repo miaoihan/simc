69306dc925cc60390595180ba41d09fd6dea26c0
Mlklock <118071530+Mlklock@users.noreply.github.com>
2024-11-18 23:52:00 -0300

    [Warlock] [Demonology] Update AP/SP conversion rates for Dreadstalkers (#9748)
    * Update sc_warlock_pets.cpp
    
    * Update sc_warlock_pets.cpp
    
    Updated to remove the changes towards Vilefiend, the 20% damage increase was coming from a talent who increases the conversion rates by 20%.
    
    * Update sc_warlock_pets.cpp
    
    * Adjust comment to reflect update date
    
    ---------
    
    Co-authored-by: Azevara <46403881+Azevara@users.noreply.github.com>

--- engine/class_modules/warlock/sc_warlock_pets.cpp
+++ engine/class_modules/warlock/sc_warlock_pets.cpp
@@ -1264,8 +1264,8 @@ dreadstalker_t::dreadstalker_t( warlock_t* owner ) : warlock_pet_t( owner, "drea
   action_list_str = "leap/dreadbite";
   resource_regeneration  = regen_type::DISABLED;
 
-  // 2023-09-20: Coefficient updated
-  owner_coeff.ap_from_sp = 0.686;
+  // 2024-11-16: Coefficient updated
+  owner_coeff.ap_from_sp = 0.825;
 
   owner_coeff.health = 0.4;
 
